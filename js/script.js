447ec3356855fb041985a1ea7bfc81e0c097cff6
ahmed gamal <114102777+ahmedgamaleid@users.noreply.github.com>
2023-09-05 19:45:05 +0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,4 +1,29 @@
-  var mysong =document.getElementById("mysong");
+document.addEventListener("DOMContentLoaded", function () {
+    const btn = document.getElementById("button");
+		const form = document.getElementById("send");
+        form.addEventListener("submit", function (event) {
+            event.preventDefault();
+
+            btn.value = "Sending...";
+
+            const templateID = "template_9e8sjnv";
+            const serviceID = "gmail";
+
+            emailjs.sendForm(serviceID, templateID, this).then(
+                () => {
+                    btn.value = "Send Email";
+                    event.reset();
+                },
+                (err) => {
+                    btn.value = "Send Email";
+                    alert(JSON.stringify(err));
+                }
+            );
+            form.reset();
+        });
+});
+////////////////////////
+    var mysong =document.getElementById("mysong");
   var icon =document.getElementById("icon");//  var mysong =document.getElementById("mysong"); var myimg= document.getElementsByClassName('myimg');
 icon.onclick = function(){
 if(mysong.paused){
